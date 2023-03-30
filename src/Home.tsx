11b5e048ab55a53c7e0300157139ce4d29d56e06
Petromir <petromir@devlabs.bg>
2023-03-30 18:27:48 +0300

    change: styles, add: styled-components

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,239 +1,222 @@
-import { useState, useCallback } from "react";
-import { observer } from "mobx-react";
-import Dropzone from "react-dropzone";
-import Metamask from "./components/MetaMask";
-import eth from "./stores/eth";
-import { InsertDriveFileOutlined, AttachFile } from "@material-ui/icons";
-import parseEmail from "../parse-email/browser";
-import toSolidity from "../parse-email/utils/toSolidity";
+import { useState, useCallback } from 'react'
+import styled from 'styled-components'
+import { observer } from 'mobx-react'
+import Dropzone from 'react-dropzone'
+import Metamask from './components/MetaMask'
+import eth from './stores/eth'
+import { InsertDriveFileOutlined, AttachFile } from '@material-ui/icons'
+import parseEmail from '../parse-email/browser'
+import toSolidity from '../parse-email/utils/toSolidity'
+import GlobalStyle from './styles'
 
 const verify = (email: string): Promise<any> => {
-  return new Promise(async (resolve, reject) => {
-    const DKIM = await eth.getContract("DKIM").catch(reject);
+	return new Promise(async (resolve, reject) => {
+		const DKIM = await eth.getContract('DKIM').catch(reject)
 
-    const dkims = await parseEmail(email);
+		const dkims = await parseEmail(email)
 
-    const dkimsInBytes = dkims.map(dkim => {
-      return toSolidity({
-        algorithm: dkim.algorithm,
-        hash: dkim.hash,
-        signature: dkim.signature.signature,
-        exponent: dkim.exponent,
-        modulus: dkim.modulus,
-        signatureData: dkim.signature
-      });
-    });
+		const dkimsInBytes = dkims.map((dkim) => {
+			return toSolidity({
+				algorithm: dkim.algorithm,
+				hash: dkim.hash,
+				signature: dkim.signature.signature,
+				exponent: dkim.exponent,
+				modulus: dkim.modulus,
+				signatureData: dkim.signature
+			})
+		})
 
-    const result = Promise.all(
-      dkimsInBytes.map((dkim, i) => {
-        return DKIM.verify(
-          dkim.algorithm,
-          dkim.hash,
-          dkim.signature,
-          dkim.exponent,
-          dkim.modulus
-        ).then(res => ({
-          name: dkims[i].entry.name,
-          verified: res["0"],
-          signatureData: dkim.signatureData
-        }));
-      })
-    ).catch(reject);
+		const result = Promise.all(
+			dkimsInBytes.map((dkim, i) => {
+				return DKIM.verify(dkim.algorithm, dkim.hash, dkim.signature, dkim.exponent, dkim.modulus).then((res) => ({
+					name: dkims[i].entry.name,
+					verified: res['0'],
+					signatureData: dkim.signatureData
+				}))
+			})
+		).catch(reject)
 
-    return resolve(result);
-  });
-};
+		return resolve(result)
+	})
+}
 
 const Home = observer(() => {
-  const [email, setEmail] = useState(null);
-  const [error, setError] = useState(null);
-  const [verified, setVerified] = useState([]);
-  const [dragging, setDrag] = useState(false);
-  const disabled =
-    !eth.isInstalled ||
-    !(eth.network === "sepolia" || eth.network === "unknown network");
+	const [email, setEmail] = useState(null)
+	const [error, setError] = useState(null)
+	const [verified, setVerified] = useState([])
+	const [dragging, setDrag] = useState(false)
+	const disabled = !eth.isInstalled || !(eth.network === 'sepolia' || eth.network === 'unknown network')
 
-  const onDrop = useCallback(files => {
-    setEmail(null);
-    setError(null);
-    setVerified([]);
+	const onDrop = useCallback((files) => {
+		setEmail(null)
+		setError(null)
+		setVerified([])
 
-    if (files.length < 1) {
-      return setError("no file included");
-    }
-    const file = files[0];
+		if (files.length < 1) {
+			return setError('no file included')
+		}
+		const file = files[0]
 
-    const reader = new FileReader();
+		const reader = new FileReader()
 
-    reader.onabort = () => setError("file reading was aborted");
-    reader.onerror = () => setError("file reading has failed");
-    reader.onload = () => setEmail(reader.result);
+		reader.onabort = () => setError('file reading was aborted')
+		reader.onerror = () => setError('file reading has failed')
+		reader.onload = () => setEmail(reader.result)
 
-    reader.readAsText(file);
-  }, []);
+		reader.readAsText(file)
+	}, [])
 
-  return (
-    <div className="container">
-      <h2>solidity-dkim demo</h2>
+	return (
+		<>
+			<GlobalStyle />
+			<Container>
+				<Body>
+					<Title>solidity-dkim demo</Title>
 
-      <Metamask />
-      <Dropzone
-        onDrop={onDrop}
-        multiple={false}
-        onDragEnter={() => setDrag(true)}
-        onDragLeave={() => setDrag(false)}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div
-              {...getRootProps({
-                style: {
-                  width: "50vw",
-                  height: "30vh",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  display: "flex",
-                  border:
-                    dragging || email
-                      ? "3px solid #387fc7"
-                      : "3px dashed #387fc7",
-                  backgroundColor: "white",
-                  margin: "50px",
-                  borderRadius: "3px",
-                  cursor: "pointer"
-                }
-              })}
-            >
-              <input {...getInputProps()} />
-              {email ? (
-                <div className="fileUploaded">
-                  <InsertDriveFileOutlined
-                    style={{ height: "8vh", width: "auto", color: "#397ec7" }}
-                  />{" "}
-                  FILE LOADED
-                </div>
-              ) : (
-                <div className="fileUploaded">
-                  <AttachFile
-                    style={{ height: "8vh", width: "auto", color: "#397ec7" }}
-                  />
-                  <p>Drag 'n' drop some files here, or click to select files</p>
-                </div>
-              )}
-            </div>
-          </section>
-        )}
-      </Dropzone>
+					<Metamask />
+					<Dropzone
+						onDrop={onDrop}
+						multiple={false}
+						onDragEnter={() => setDrag(true)}
+						onDragLeave={() => setDrag(false)}>
+						{({ getRootProps, getInputProps }) => (
+							<DragNDrop {...getRootProps()}>
+								<input {...getInputProps()} />
+								{email ? (
+									<FileStatus>
+										<InsertDriveFileOutlined /> FILE LOADED
+									</FileStatus>
+								) : (
+									<FileStatus>
+										<AttachFile />
+										<p>Drag 'n' drop some files here, or click to select files</p>
+									</FileStatus>
+								)}
+							</DragNDrop>
+						)}
+					</Dropzone>
 
-      <button
-        onClick={() =>
-          verify(email)
-            .then(setVerified)
-            .catch(setError)
-        }
-        disabled={disabled}
-      >
-        Verify
-      </button>
-      {error ? (
-        <p className="error">error: {error}</p>
-      ) : verified.length > 0 ? (
-        verified.map(result =>
-          result.verified ? (
-            <div className="verified__wrapper">
-              <h1 key={result.name} className="verified">
-                {result.name}: verified! 🎉
-              </h1>
-              <p className="verified__signature">
-                <b>Signature:</b> {result.signatureData.signature}
-              </p>
-              <p>
-                <b>Domain:</b> {result.signatureData && result.signatureData.domain}
-              </p>
-              <p>
-              <b>Expires:</b> {result.signatureData && new Date (Number(result.signatureData.expires + 1000)).toLocaleString('en-US', {
-                  weekday: 'long',
-                  day: 'numeric', 
-                  month: "2-digit", 
-                  year: "numeric"
-                })}
-              </p>
-            </div>
-          ) : (
-            <h1 key={result.name} className="not-verified">
-              {result.name}: not verified 😔
-            </h1>
-          )
-        )
-      ) : (
-        ""
-      )}
+					<VerifyButton onClick={() => verify(email).then(setVerified).catch(setError)} disabled={disabled}>
+						Verify
+					</VerifyButton>
+					{error ? (
+						<p className="error">error: {error}</p>
+					) : verified.length > 0 ? (
+						verified.map((result) =>
+							result.verified ? (
+								<div className="verified__wrapper">
+									<h1 key={result.name} className="verified">
+										{result.name}: verified! 🎉
+									</h1>
+									<p className="verified__signature">
+										<b>Signature:</b> {result.signatureData.signature}
+									</p>
+									<p>
+										<b>Domain:</b> {result.signatureData && result.signatureData.domain}
+									</p>
+									<p>
+										<b>Expires:</b>{' '}
+										{result.signatureData &&
+											new Date(Number(result.signatureData.expires + 1000)).toLocaleString('en-US', {
+												weekday: 'long',
+												day: 'numeric',
+												month: '2-digit',
+												year: 'numeric'
+											})}
+									</p>
+								</div>
+							) : (
+								<h1 key={result.name} className="not-verified">
+									{result.name}: not verified 😔
+								</h1>
+							)
+						)
+					) : (
+						''
+					)}
+				</Body>
+			</Container>
+		</>
+	)
+})
 
-      <style jsx global>{`
-        html {
-          background: aliceblue;
-        }
-       
-        `}</style>
+export default Home
 
-      <style jsx>{`
-        .container {
-          height: 100%;
-          box-sizing: border-box;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: flex-start;
-          margin-bottom: 20px;
-        }
-        h2 {
-          margin-top: 100px;
-          margin-bottom: 30px;
-        }
-        button {
-          height: 30px;
-          width: 120px;
-          border-radius: 3px;
-          background: #397ec7;
-          color: white;
-          border: none;
-          font-family: sans-serif;
-          font-size: 16px;
-          cursor: pointer;
-          opacity: ${disabled ? "0.5" : "1"};
-        }
-        .error {
-          color: #d60000;
-        }
-        .verified__wrapper {
-          margin-top: 50px;
-          max-width: 50%;
-          padding: 5px;
-          border: 1px solid rgb(56, 127, 199);
-          border-radius: 5px;
-        }
-        .verified__wrapper h1 {
-          margin-bottom: 0;
-        }
-        .verified__signature {
-          word-break: break-all;
-        }
-        .verified {
-          color: #00bb39;
-        }
-        .not-verified {
-          color: #d60000;
-        }
-        .fileUploaded {
-          height: 12vh;
-          justify-content: center;
-          align-items: center;
-          display: flex;
-          flex-direction: column;
-        }
-      `}</style>
-    </div>
-  );
-});
+const Container = styled.div`
+	padding: 2.5rem;
+	width: 100%;
+	min-height: 100vh;
+	background: hsla(331, 78%, 69%, 1);
+	background: linear-gradient(90deg, hsla(331, 78%, 69%, 1) 0%, hsla(238, 82%, 70%, 1) 100%);
+	background: -moz-linear-gradient(90deg, hsla(331, 78%, 69%, 1) 0%, hsla(238, 82%, 70%, 1) 100%);
+	background: -webkit-linear-gradient(90deg, hsla(331, 78%, 69%, 1) 0%, hsla(238, 82%, 70%, 1) 100%);
+	filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#ED71AD", endColorstr="#7277F1", GradientType=1 );
+`
 
-export default Home;
+const Body = styled.div`
+	max-width: 80rem;
+	padding: 2.5rem;
+	margin: 0 auto;
+
+	// Glass effect
+	background: rgba(255, 255, 255, 0.2);
+	border-radius: 12px;
+	box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+	backdrop-filter: blur(5px);
+	-webkit-backdrop-filter: blur(5px);
+	border: 1px solid rgba(255, 255, 255, 0.3);
+`
+
+const DragNDrop = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+  margin-bottom: 4rem;
+  min-height: 300px;
+  cursor: pointer;
+
+	// Glass effect
+	background: rgba(255, 255, 255, 0.53);
+	border-radius: 12px;
+	box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+	backdrop-filter: blur(5px);
+	-webkit-backdrop-filter: blur(5px);
+	border: 1px solid rgba(255, 255, 255, 0.3);
+`
+
+const FileStatus = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  
+  svg {
+    color: #303030;
+    width: 3.125rem;
+    height: 3.125rem;
+  }
+
+  p {
+    color: #303030;
+  }
+`
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 2rem;
+  color: #fff;
+  margin-bottom: 2.2rem;
+`
+
+const VerifyButton = styled.button`
+  width: 100%;
+  padding: 1.25rem;
+  border-radius: 12px;
+  border: none;
+  background-color: #fff;
+  color: #303030;
+  font-size: 1rem;
+  font-weight: 600;
+`
--- src/components/MetaMask.tsx
+++ src/components/MetaMask.tsx
@@ -1,71 +1,61 @@
-import { observer } from "mobx-react";
-import ethStore from "../stores/eth";
+import { observer } from 'mobx-react'
+import styled from 'styled-components'
+import ethStore from '../stores/eth'
 
-const GetText = ({
-  isInstalled,
-  isLoggedIn,
-  account,
-  network
-}: typeof ethStore) => {
-  if (!isInstalled) {
-    return (
-      <a
-        href="https://metamask.io/"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ color: "#387fc7", textDecoration: "none" }}
-      >
-        INSTALL METAMASK
-      </a>
-    );
-  }
+const Text = ({ isInstalled, account, network }: typeof ethStore) => {
+	if (!isInstalled) {
+		return (
+			<a
+				href="https://metamask.io/"
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ color: '#387fc7', textDecoration: 'none' }}>
+				INSTALL METAMASK
+			</a>
+		)
+	}
 
-  return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      {account ? (
-        <div style={{ paddingRight: "1vh" }}>{`[${account}]`}</div>
-      ) : null}
+	return (
+		<div style={{ display: 'flex', flexDirection: 'row' }}>
+			{account ? <div style={{ paddingRight: '1vh' }}>{`[${account}]`}</div> : null}
 
-      <div>{network}</div>
-    </div>
-  );
-};
+			<div>{network}</div>
+		</div>
+	)
+}
 
 const Metamask = observer(() => {
-  const { isLoggedIn } = ethStore;
+	return (
+		<Wrapper>
+			<Image src="/static/images/metamask.png" alt="Metamask Logo" className="img" />
+			<Text
+        {...ethStore}
+			/>
+		</Wrapper>
+	)
+})
 
-  return (
-    <div className="metamaskStatus">
-      <img
-        src="/static/images/metamask.png"
-        alt="Metamask Logo"
-        className="img"
-      />
-      {GetText(ethStore)}
+export default Metamask
 
-      <style jsx>{`
-        .img {
-          width: 4vh;
-          height: 4vh;
-          padding-right: 1vh;
-        }
-        .metamaskStatus {
-          background-color: #fafafa;
-          border: 1px solid ${isLoggedIn ? "#56A2BA" : "#387fc7"};
-          color: ${isLoggedIn ? "#56A2BA" : "#387fc7"};
-          border-radius: 3px;
-          padding-left: 2vh;
-          padding-right: 2vh;
-          font-size: calc(12px + 0.4vw);
-          height: 4.5vh;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-evenly;
-          align-items: center;
-        }
-      `}</style>
-    </div>
-  );
-});
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: .5rem;
+  margin-bottom: 1.2rem;
+  border-radius: 12px;
+  color: #303030;
 
-export default Metamask;
+  // Glass effect
+  background: rgba(255, 255, 255, 0.42);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+`
+
+const Image = styled.img`
+  width: 3rem;
+  height: 3rem;
+`
