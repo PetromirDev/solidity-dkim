8e359de5d0f79c0848020e68fed92fc39b5b84ba
Petromir <petromir@devlabs.bg>
2023-03-31 12:47:48 +0300

    add: raw-loader, webpack

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-    emotion: true
-  },
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+		emotion: true
+	},
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+		// Important: return the modified config
+		config.module.rules.push({ test: /\.eml$/, use: 'raw-loader' })
+
+		return config
+	}
 }
 
 module.exports = nextConfig
